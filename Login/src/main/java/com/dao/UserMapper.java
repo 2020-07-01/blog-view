ca6552db3277622124cc0b1a3e3fab7dc124b97f
yuqiang <2422321558@qq.com>
2019-04-21 20:44:53 +0800

    dao层

--- Login/src/main/java/com/dao/UserMapper.java
+++ Login/src/main/java/com/dao/UserMapper.java
@@ -2,17 +2,27 @@ package com.dao;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
-
 import com.entity.User;
+
 @Repository
 public interface UserMapper {
+
+	/*
+	 * @Description:根据用户名和密码进行注册 
+	 * @param username：用户名
+	 * @param password：用户密码
+	 * @Param注解的作用是给参数值命名，参数命名后就可以根据名字将参数值正确的写到sql语句中
+	 */
+	public void addUser(@Param("username") String username, @Param("password") String password);
+
+	/*
+	 * @Description:根据用户名查询密码进行登陆验证 
+	 * @param name：用户名
+	 * @return：返回用户的数据
+	 */
+	public User selectPasswordByName(String name);
 	
-	   //根据用户名和密码查找。mybatis中有多个参数时，需要使用@Param注解
-    User findByUserNameAndPassword(@Param("username")String username,@Param("password")String password);
-    //增加用户
-    void addUser(User user);
-   //根据用户名查询
-   User findByUserName(String username);
-     
-   
+	
+	
+
 }
